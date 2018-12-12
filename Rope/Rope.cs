11265eab514a45044a84e8e9f1cd16e55890b225
LAPTOP-INNV7NA1\Game Taiko <zainal@gametaiko.com>
2018-12-12 20:06:42 +0800

    Rope Components

---
+++ Rope/Rope.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour {
+
+    GameObject obj;
+    public int howMany=1;
+	// Use this for initialization
+	void Start () {
+
+        GameObject temp = null;
+        for (int i=0;i < howMany; i++)
+        {
+            obj = transform.GetChild(transform.childCount - 1).gameObject;
+            temp = Instantiate(obj, transform) as GameObject;
+            temp.GetComponent<HingeJoint>().connectedBody = obj.GetComponent<Rigidbody>();
+            temp.name = "rope";
+        }
+
+        transform.GetChild(0).GetComponent<HingeJoint>().connectedBody = temp.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
